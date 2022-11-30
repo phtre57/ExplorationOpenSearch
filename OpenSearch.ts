3491bee796d1dc61e5948fffdfb9ac584f396b85
phtre57 <phtre57@gmail.com>
2022-11-30 09:31:19 -0500

    added bulk

--- OpenSearch.ts
+++ OpenSearch.ts
@@ -21,20 +21,30 @@ export class OpenSearch {
     this.port = port
 
     this.client = new Client({
-      node: protocol + "://" + auth + "@" + host + ":" + port,
+      node: `${protocol}://${auth}@${host}:${port}`,
+      ssl: {
+        rejectUnauthorized: false,
+      }
     });
   }
 
-  addDocument(index: string, doc: Record<any, any>) {
-    this.client.index({
+  async addDocument(index: string, doc: Record<any, any>): Promise<any> {
+    return await this.client.index({
       index: index,
       body: doc,
       refresh: true,
     })
   }
 
-  search(index: string, query: Record<any, any>) {
-    this.client.search({
+  async bulkCreateAtIndex(index: string, documents: any[]) {
+    return await this.client.bulk({
+      index: index,
+      body: documents,
+    })
+  }
+
+  async search(index: string, query: Record<any, any>): Promise<any> {
+    return await this.client.search({
       index: index,
       body: query,
     })
--- main.ts
+++ main.ts
@@ -2,27 +2,42 @@ import fs from 'fs'
 
 import { OpenSearch } from './OpenSearch'
 
-const dataPath = '../funds.json'
+const fundsPath = '../funds.json'
+const sectorsPath = '../funds_sectors.json'
 
 const host = "localhost"
 const protocol = "https"
 const port = 9200
 const auth = "admin:admin"
 
-console.log('Opening funds...')
-const raw = fs.readFileSync(dataPath);
-const funds = JSON.parse(raw.toString());
+;(async () => {
+  console.log('Opening funds...')
+  const rawFunds = fs.readFileSync(fundsPath)
+  const funds = JSON.parse(rawFunds.toString())
 
-const openSearch = new OpenSearch({
-  auth: auth,
-  host: host,
-  port: port,
-  protocol: protocol,
-})
+  console.log('Opening sectors...')
+  const rawSectors = fs.readFileSync(sectorsPath)
+  const sectors = JSON.parse(rawSectors.toString())
 
-console.log('Adding all funds to index...')
+  const openSearch = new OpenSearch({
+    auth: auth,
+    host: host,
+    port: port,
+    protocol: protocol,
+  })
 
-funds.forEach((fund: any) => openSearch.addDocument('funds', fund))
+  // console.log('Adding all funds to index...')
 
-console.log('Finished adding all funds to index...')
+  // funds.forEach(async (fund: any) => await openSearch.addDocument('funds', fund))
+
+  // console.log('Finished adding all funds to index...')
+
+  console.log('Adding all sectors to index...')
+
+  await openSearch.addDocument('sectors', sectors[0])
+
+  // await openSearch.bulkCreateAtIndex('sectors', sectors)
+
+  console.log('Finished adding all sectors to index...')
+})()
 
